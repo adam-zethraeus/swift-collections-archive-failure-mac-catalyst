36b01abae8773192812f676c4537dc528bae3d73
adamz <adamz@ambiencehealthcare.com>
2024-09-15 23:44:34 -0700

    Initial Commit

---
+++ Void/VoidApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoidApp.swift
+//  Void
+//
+//  Created by adamz on 9/15/24.
+//
+
+import SwiftUI
+
+@main
+struct VoidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VoidTests/VoidTests.swift
@@ -0,0 +1,16 @@
+//
+//  VoidTests.swift
+//  VoidTests
+//
+//  Created by adamz on 9/15/24.
+//
+
+import Testing
+
+struct VoidTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
